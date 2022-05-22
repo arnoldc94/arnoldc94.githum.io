0a435e321170de8d16a0f888da3b150cf9f7d2c8
arnoldc94 <84541944+arnoldc94@users.noreply.github.com>
2022-05-22 16:42:06 -0700

    Add files via upload

---
+++ treasureHunt.js
@@ -0,0 +1,65 @@
+
+//function sortTable() {
+//let table = document.getElementById(9);
+//let dataCollection = table.querySelector("td");
+//let data = Array.from(dataCollection);
+//shuffleArray(data);
+//for(const row of rows) {
+//  table.appendChild(row)
+  
+
+
+
+//function shuffleArray(array){
+//  for (var i = ){
+//    
+//  }
+//}
+
+
+
+const treasure = (location) => {
+
+  if (location === 0 ){
+    document.getElementById(0).innerHTML = "🏴‍☠️"
+
+    alert("You lose");
+    
+  }else if(location === 1 ){
+    alert("you win")
+    document.getElementById(1).innerHTML = "⚓️"
+
+    
+
+  }else if ( location === 2 ){
+    
+    document.getElementById(2).innerHTML = "🏝"
+    
+  } else if ( location === 3 ){
+    
+    document.getElementById(3).innerHTML = "🏝"
+    
+  } else if ( location === 4 ){
+    
+    document.getElementById(4).innerHTML = "🏝"
+    
+  } else if ( location === 5 ){
+    
+    document.getElementById(5).innerHTML = "🏝"
+    
+  } else if ( location === 6 ){
+    
+    document.getElementById(6).innerHTML = "🏝"
+    
+  } else if ( location === 7 ){
+    
+    document.getElementById(7).innerHTML = "🏝"
+    
+  } else if ( location === 8 ){
+    
+    document.getElementById(8).innerHTML = "🏝"
+    
+  }
+}
+
+
